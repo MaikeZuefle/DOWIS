ce2aad2f2b83406a9f261b441735685899c5485e
spapi <spapi@fbk.eu>
2026-02-05 15:20:12 +0100

    Fix MCIF SSUM

--- data/ssum.py
+++ data/ssum.py
@@ -1,29 +1,29 @@
 import os
 import gzip
-import json
 
 import urllib.request
 import xml.etree.ElementTree as ET
 
-MCIF_URL = "https://huggingface.co/datasets/FBK-MT/MCIF/resolve/main/"
+MCIF_DOWNLOAD_URL = "https://huggingface.co/datasets/FBK-MT/MCIF/resolve/main/"
+MCIF_URL = "https://huggingface.co/datasets/FBK-MT/MCIF/tree/main/MCIF_DATA/LONG_AUDIOS"
 
 def download_audio(base_dir):
-    api_url = "https://huggingface.co/api/datasets/FBK-MT/MCIF/tree/main"
+    with urllib.request.urlopen(MCIF_URL) as r:
+        html = r.read().decode("utf-8")
 
-    with urllib.request.urlopen(api_url) as response:
-        files = json.load(response)
+    files = []
+    for part in html.split('"'):
+        if part.endswith(".wav"):
+            files.append(os.path.basename(part))
 
-    wav_files = [f["path"] for f in files if f["path"].endswith(".wav")]
-
-    for file_path in wav_files:
-        filename = os.path.basename(file_path)
+    print("Downloading audio...")
+    for filename in sorted(set(files)):
         out_path = os.path.join(base_dir, filename)
-
         if os.path.exists(out_path):
             continue
-
-        download_url = MCIF_URL + file_path
-        urllib.request.urlretrieve(download_url, out_path)
+        urllib.request.urlretrieve(
+            f"{MCIF_DOWNLOAD_URL+"MCIF_DATA/LONG_AUDIOS"}/{filename}", out_path)
+    print("Done.")
 
 
 def load_ssum(language):
@@ -36,7 +36,7 @@ def load_ssum(language):
     download_audio(base_dir)
 
     file_name = f"MCIF.long.{language}.ref.xml.gz"
-    url = MCIF_URL + f"{file_name}?download=true"
+    url = MCIF_DOWNLOAD_URL + f"{file_name}?download=true"
 
     audio_paths = []; references = []
 
@@ -46,7 +46,7 @@ def load_ssum(language):
 
             for event, elem in context:
                 if elem.tag == "sample" and elem.attrib.get("task") == "SUM":
-                    audio_paths.append(elem.findtext("audio_path"))
+                    audio_paths.append(base_dir + "/" + elem.findtext("audio_path"))
                     references.append(elem.findtext("reference"))
                     elem.clear()
     return {"inputs": audio_paths, "references": references}
